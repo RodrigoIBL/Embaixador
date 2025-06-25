7c0b113b23d663bcbd9ca08f63e7cfb311b1c04a
RodrigoIBL <rodrigo.carvalho@ibloom.digital>
2025-06-25 21:25:09 +0100

    Update Winners.php

--- Winners.php
+++ Winners.php
@@ -23,28 +23,16 @@ function cmp_add_reservas_menu() {
      // Add subpages
      add_submenu_page(
         'cmp-winners',          // Parent slug
-        'Ver recompensas',           // Page title
-        'Ver recompensas',           // Menu title
+        'Ver reservas',           // Page title
+        'Ver reservas',           // Menu title
         'manage_options',       // Capability
         'cmp-ver_recompensas',       // Menu slug
         'cmp_ver_recompensas'   // Function to display the subpage content
-    );
-    add_submenu_page(
-        'cmp-winners',          // Parent slug
-        'Adicionar Progresso',           // Page title
-        'Adicionar Progresso',           // Menu title
-        'manage_options',       // Capability
-        'cmp-adicionar_progresso',       // Menu slug
-        'cmp_adicionar_progresso'   // Function to display the subpage content
     );
 }
 
 // Hook the function to 'admin_menu' action
 add_action('admin_menu', 'cmp_add_reservas_menu');
-
-function buscar_dados() {
-    
-}
 
 // Function to display the "Winners" page content
 function cmp_winners_page() {
