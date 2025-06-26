e2c6b4b096a8fa87e29e5318d08c40f42fc9b155
RodrigoIBL <rodrigo.carvalho@ibloom.digital>
2025-06-26 23:02:00 +0100

    a

--- Reservas.php
+++ Reservas.php
@@ -37,7 +37,7 @@ function cmp_reservas_page()
     $api_url = 'https://app.hostkit.pt/api/getReservations?APIKEY=5aQElqgU34RIgKDsKxIfuqzjVFR7eH8XxUgZ1StjpcD3rTrJRI';
     $response = wp_remote_get($api_url);
 
-    echo '<div class="wrap"><h1>Reservas</h1>';
+    echo '<div class="wrap"><h1>Reservas 1</h1>';
 
     if (is_wp_error($response)) {
         echo '<div class="notice notice-error"><p>Erro ao aceder à API: ' . esc_html($response->get_error_message()) . '</p></div>';
